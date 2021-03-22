477f9a3dd5de7745faf787ea319e47124ee25a09
nmduc <nguyenduc.tn.94@gmail.com>
2021-03-23 00:36:11 +0700

    Add function to gen uuid and interactive with localStorage

--- src/services/index.js
+++ src/services/index.js
@@ -7,3 +7,25 @@ export const isValidUser = (username, password) => {
 
   return !!user;
 };
+
+export const generateID = () => {
+  let d = new Date().getTime();
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+};
+
+export const savePostToLocalStorage = (username, listPost) => {
+  const stringListPost = JSON.stringify(listPost);
+  localStorage.setItem(username, stringListPost);
+};
+
+export const getPostFromLocalStorage = (username) => {
+  const stringListPost = !!localStorage.getItem(username)
+    ? localStorage.getItem(username)
+    : "[]";
+
+  return JSON.parse(stringListPost);
+};
