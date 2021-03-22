664c637f891092dcb3dffcaec2fcf244d67c5ade
nmduc <nguyenduc.tn.94@gmail.com>
2021-03-22 21:39:00 +0700

    Add route to main page

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,11 +1,27 @@
 import React, { useContext } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Login from "./Login";
+import PostManagement from "./PostManagement";
+
+const RestrictRoute = ({ path, isAuth, component: Component }) => (
+  <Route
+    path={path}
+    render={isAuth ? <Component /> : <Redirect to="/login" />}
+  />
+);
 
 const Main = () => {
-  console.log(" Go to main");
-  return <Login />;
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <RestrictRoute
+        path="/post-management"
+        component={PostManagement}
+        isAuth={true}
+      />
+    </Switch>
+  );
 };
 
 export default Main;
